7fff7e781ff36d4bf329281db5e73804fd55d114
SaintAM <113990489+SaintAM@users.noreply.github.com>
2022-09-22 18:57:42 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const nav = document.querySelector('#nav');
+const navBtn = document.querySelector('#nav-btn');
+const navBtnImg = document.querySelector('#nav-btn-img');
+
+navBtn.onclick = () => {
+	if (nav.classList.toggle('nav-burger')) {
+		navBtnImg.src = "./img/nav-close.svg";
+	} else {
+		navBtnImg.src = "./img/nav-open.svg";
+	}
+}
+
+AOS.init();
